ecfed57444300a2f5b324d52235a97113271ef73
Marcos Oliveira <marcos@terminalroot.com.br>
2025-12-29 17:01:58 -0300

    No message for this commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-  std::cout << "Terminal Net!\n";
+  std::cout << "Terminal Net! Update 2025-12-29\n";
   std::cout << "Removido Kwallet: git config --global --unset credential.helper\n";
   return EXIT_SUCCESS;
 }
